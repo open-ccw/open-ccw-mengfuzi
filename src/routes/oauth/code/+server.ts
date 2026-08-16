56e890660b0bf92c82f9f8cc9746ccc211f20510
BenPaoDeXiaoZhi <mengfanzhi1232022@163.com>
2026-08-16 20:52:36 +0800

    use sveltekit env

--- src/routes/oauth/code/+server.ts
+++ src/routes/oauth/code/+server.ts
@@ -2,16 +2,15 @@ import { error, text } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 import { oauthState } from "../oauthConfig";
 import CryptoJS from "crypto-js";
-import { env } from "cloudflare:workers";
 
-export const POST: RequestHandler = async ({ request }) => {
+export const POST: RequestHandler = async ({ request, platform }) => {
   const { code } = await request.json<{ code?: string }>();
   if (!code) {
     return error(500, "code is required");
   }
   const key = CryptoJS.enc.Utf8.parse(oauthState.substring(0, 16));
   const decrypted = CryptoJS.AES.decrypt(code, key, {
-    iv: CryptoJS.enc.Utf8.parse(env.SEC_OAUTH_IV),
+    iv: CryptoJS.enc.Utf8.parse(platform!.env.SEC_OAUTH_IV),
   }).toString(CryptoJS.enc.Utf8);
   return text(decrypted);
 };
