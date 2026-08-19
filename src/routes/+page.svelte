a4e1f7a515f3b379d966292cfdf67013d44e2b48
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-08-19 08:39:54 +0800

    更新 +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,7 +28,7 @@
   <title>Open CCW-第三方 CCW 启动器</title>
   <meta
     name="description"
-    content="Open CCW 是一款轻量化的第三方 CCW 社区客户端，基于svelte框架，支持 OAuth 一键授权登录、多账号管理、AES 本地加密存储 Token、每日签到领金币。是一款以安全为主要理念，便捷实用的 CCW 第三方启动器。"
+    content="Open CCW 是一款轻量化的第三方 CCW 社区客户端，基于svelte框架，支持 OAuth 一键授权登录、多账号管理、AES 本地加密存储 Token、每日签到领金币。是一款以安全为主要理念，便捷实用的 CCW 第三方启动器。同时提供了pwa支持。"
   />
   <meta
     name="keywords"
